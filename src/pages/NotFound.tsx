4e558db6db0d0aa02e22c46d3a66aac328eebdbe
teDdyMucho <123669239+teDdyMucho@users.noreply.github.com>
2025-04-04 03:49:18 +0800

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 const NotFound = () => {
   const location = useLocation();
 
+  
   useEffect(() => {
     console.error(
       "404 Error: User attempted to access non-existent route:",
